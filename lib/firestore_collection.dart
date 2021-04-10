8a18c66381ebbffe73e86eb9a7700e4466b56ade
indy <ingo.esn@gmx.de>
2021-04-10 14:56:50 +0200

    fixed issue that live collections didn't use cache

--- lib/firestore_collection.dart
+++ lib/firestore_collection.dart
@@ -208,8 +208,9 @@ class FirestoreCollection {
 
       } else {
         // if the latest validated server value is less than the local: refetch from server
-        if (_latestValidatedServerFetch == null ||
-            _lastFetched() <= _latestValidatedServerFetch) {
+        if (!live &&
+            (_latestValidatedServerFetch == null ||
+                _lastFetched() <= _latestValidatedServerFetch)) {
           QuerySnapshot serverQS = await _q
               .where(queryOrder.orderField, isLessThan: _lastFetched())
               .where(queryOrder.orderField,
